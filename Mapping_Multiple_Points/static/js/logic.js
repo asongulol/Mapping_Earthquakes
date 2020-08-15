b1e8e3a73f10c83e421104a86c0f5c299d1044ce
asongulol <otrinidad@abckidsny.com>
2020-08-15 17:49:02 -0400

    done with skill drill

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -7,20 +7,32 @@ console.log("working");
 
 // Create the map object with a  
 // center = [34.05, -118.24] and zoom level = 14
-let map = L.map("mapid").setView([34.0522, -118.2437], 14);
+let map = L.map("mapid").setView([37.0902, -95.7129], 4);
 
+// Get data from cities.js
+let cityData = cities;
+
+// Loop through the cities array and create one marker for each city 
+//.bindPopup defines the popup text, .toLocaleString() creates thousands separator
+cityData.forEach(function(city) {
+	console.log(city)
+	L.circleMarker(city.location, {
+		radius: city.population/100000,
+		color: 'orange',
+		fillColor: '#ffed83',
+		fillOpacity: 0.5
+	})
+	.bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+  .addTo(map);
+});
 // Add circle coordinates = [], line color = 
 // circle vs circleMarker -> the radius is meters vs pixel meaning the size of the marker becomes bigger as you zoom out
-L.circleMarker([34.0522, -118.2437], {
-    color: 'black',
-    fillColor: '#ffed83',
-    fillOpacity: 0.5,
-    radius: 300
-}).addTo(map);
-
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.marker([34.0522, -118.2437]).addTo(map);
-
+// L.circle([34.0522, -118.2437], {
+//     color: 'black',
+//     fillColor: '#ffed83',
+//     fillOpacity: 0.5,
+//     radius: 100
+// }).addTo(map);
 
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
